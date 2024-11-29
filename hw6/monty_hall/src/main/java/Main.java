d703148876ec22fdee01077c436c9fe9bb3762cc
Mihail Nemirovchenko <nemirovchenko@gmail.com>
2024-11-29 20:47:26 +0300

    t

--- hw6/monty_hall/src/main/java/Main.java
+++ hw6/monty_hall/src/main/java/Main.java
@@ -3,56 +3,45 @@ import java.util.Map;
 import java.util.Random;
 
 public class Main {
+    
+    private static Random random = new Random();
+    private static Map<Integer,Boolean> results = new HashMap<>();
+    private static int count = 3;
+    
     public static void main(String[] args) {
-        Studio studio = new Studio(5, 2);
-        System.out.println(studio);
+
+        for (int i = 0; i < 1000; i++) {
+            game(i);
+        }
+
+        int win = 0;
+        for (Map.Entry<Integer, Boolean> entry: results.entrySet()){
+            if (entry.getValue()){
+                win++;
+            }
+        }
+        
+        System.out.println("Количество побед игрока = " + win);
     }
 
-    //    static Random random;
-//    static Map<Integer,Boolean> results;
-//    static int count;
-//
-//    public static void main(String[] args) {
-//
-//        random = new Random();
-//        results = new HashMap<>();
-//        count = 3;
-//
-//
-//        for (int i = 0; i < 1000; i++) {
-//            game(i);
-//        }
-//
-//
-//        int vic = 0;
-//        for (Map.Entry<Integer, Boolean> entry: results.entrySet()){
-//            if (entry.getValue()){
-//                vic++;
-//            }
-//        }
-//        System.out.println("Количество побед игрока = " + vic);
-//    }
-//
-//    private static void game(int num) {
-//        int winning = random.nextInt(count);
-//        int firstDoor = random.nextInt(count);
-//        int emptyDoor = -1;
-//        int secondDoor = -1;
-//
-//
-//        for (int i = 0; i <count; i++) {
-//            if (i != winning && i != firstDoor){
-//                emptyDoor = i;
-//            }
-//        }
-//
-//
-//        for (int i = 0; i < count; i++) {
-//            if (i != emptyDoor && i != firstDoor){
-//                secondDoor = i;
-//            }
-//        }
-//
-//        results.put(num, winning == secondDoor);
-//    }
+    private static void game(int num) {
+        int winning = random.nextInt(count);
+        int firstDoor = random.nextInt(count);
+        int emptyDoor = -1;
+        int secondDoor = -1;
+
+        for (int i = 0; i <count; i++) {
+            if (i != winning && i != firstDoor){
+               emptyDoor = i;
+            }
+        }
+
+        for (int i = 0; i < count; i++) {
+            if (i != emptyDoor && i != firstDoor){
+               secondDoor = i;
+            }
+        }
+
+        results.put(num, winning == secondDoor);
+    }
 }
